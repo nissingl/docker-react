778d2c9b46ba625e6f3a4ab7764dfc61fab2635a
nishantsingla <nishant.singla@ofbusiness.com>
2020-05-04 00:20:29 +0530

    changed welcome text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          W0h00000
+          Changed for Another branch and PR creation
         </p>
         <a
           className="App-link"
